46c0c11c5f9fad60eb5c929e2f97a9872b50665e
one-for-all <jianan.li@yahoo.com>
2017-07-08 01:44:21 -0700

    refactor js & add jump to time functionality

--- js/app.js
+++ js/app.js
@@ -1,44 +1,37 @@
 $('video').mediaelementplayer({
 });
 
+const caption = document.querySelector('.caption')
+const timeCutOffs = [
+  0.24, 4.13, 7.535, 11.27, 13.96, 17.94, 22.37, 26.88, 30.92, 34.73, 39.43, 41.19, 46.30, 49.27, 53.76, 57.78, 60.15
+];
+const timeSpanMapping = {}
+for (let i = 0; i < timeCutOffs.length-1; i += 1) {
+  timeSpanMapping[timeCutOffs[i+1]] = caption.querySelectorAll('span')[i];
+}
+
 video = document.querySelector('video');
 video.addEventListener('timeupdate', () => {
   const currentTime = video.currentTime;
-  const caption = document.querySelector('.caption')
   $('.caption span').attr('class', '');
 
-  if (currentTime < 0.24) {}
-  else if (currentTime < 4.13) {
-    caption.querySelector('span:nth-of-type(1)').className = 'highlighted';
-  } else if (currentTime < 7.535) {
-    caption.querySelector('span:nth-of-type(2)').className = 'highlighted';
-  } else if (currentTime < 11.27) {
-    caption.querySelector('span:nth-of-type(3)').className = 'highlighted';
-  } else if (currentTime < 13.96) {
-    caption.querySelector('span:nth-of-type(4)').className = 'highlighted';
-  } else if (currentTime < 17.94) {
-    caption.querySelectorAll('p')[1].querySelector('span:nth-of-type(1)').className = 'highlighted';
-  } else if (currentTime < 22.37) {
-    caption.querySelectorAll('p')[1].querySelector('span:nth-of-type(2)').className = 'highlighted';
-  } else if (currentTime < 26.88) {
-    caption.querySelectorAll('p')[1].querySelector('span:nth-of-type(3)').className = 'highlighted';
-  } else if (currentTime < 30.92) {
-    caption.querySelectorAll('p')[1].querySelector('span:nth-of-type(4)').className = 'highlighted';
-  } else if (currentTime < 34.73) {
-    caption.querySelectorAll('p')[1].querySelector('span:nth-of-type(5)').className = 'highlighted';
-  } else if (currentTime < 39.43) {
-    caption.querySelectorAll('p')[1].querySelector('span:nth-of-type(6)').className = 'highlighted';
-  } else if (currentTime < 41.19) {
-    caption.querySelectorAll('p')[1].querySelector('span:nth-of-type(7)').className = 'highlighted';
-  } else if (currentTime < 46.30) {
-    caption.querySelectorAll('p')[2].querySelector('span:nth-of-type(1)').className = 'highlighted';
-  } else if (currentTime < 49.27) {
-    caption.querySelectorAll('p')[2].querySelector('span:nth-of-type(2)').className = 'highlighted';
-  } else if (currentTime < 53.76) {
-    caption.querySelectorAll('p')[2].querySelector('span:nth-of-type(3)').className = 'highlighted';
-  } else if (currentTime < 57.78) {
-    caption.querySelectorAll('p')[2].querySelector('span:nth-of-type(4)').className = 'highlighted';
-  } else if (currentTime < 60.15) {
-    caption.querySelectorAll('p')[2].querySelector('span:nth-of-type(5)').className = 'highlighted';
+  for (let i = 0; i < timeCutOffs.length; i += 1) {
+    if (currentTime < timeCutOffs[i+1]) {
+      caption.querySelectorAll('span')[i].className = 'highlighted';
+      break;
+    }
+  }
+})
+
+caption.addEventListener('click', (event) => {
+  const span = event.target;
+  if (span.tagName === 'SPAN') {
+    Object.keys(timeSpanMapping).forEach((key) => {
+      if (timeSpanMapping[key] === span) {
+        const index = timeCutOffs.indexOf(parseFloat(key));
+        const previousKey = timeCutOffs[index-1];
+        video.currentTime = previousKey;
+      }
+    })
   }
 })
